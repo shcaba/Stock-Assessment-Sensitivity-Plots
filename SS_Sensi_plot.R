fbe11bab8febe8973536dff9032e7515c8f52fe2
Jason Cope <jason.cope@noaa.gov>
2019-06-07 11:44:06 -0700

    Fix rbind in data.frame with AIC values

--- SS_Sensi_plot.R
+++ SS_Sensi_plot.R
@@ -126,7 +126,7 @@ SS_Sensi_plot<-function(model.summaries,
 	dev.quants.labs<-data.frame(c("SB0",paste0("SSB_",current.year),paste0("Bratio_",current.year),"MSY_SPR","F_SPR"),dev.quants,"Derived quantities")
 	AICs<-2*model.summaries$npars+(2*as.numeric(model.summaries$likelihoods[1,1:model.summaries$n]))
 	deltaAICs<-AICs-AICs[1]
-	AIC.out<-as.data.frame(cbind(c("AIC","deltaAIC"),rbind(AICs,deltaAICs),c("AIC")))
+	AIC.out<-data.frame(cbind(c("AIC","deltaAIC"),rbind.data.frame(AICs,deltaAICs),c("AIC")))
 	colnames(AIC.out)<-colnames(survey.lambda)<-colnames(survey.like)<-colnames(Lt.lambda)<-colnames(Lt.like)<-colnames(Age.lambda)<-colnames(Age.like)<-colnames(parms)<-colnames(dev.quants.labs)<-c("Type",mod.names,"Label")
 	Like.parm.quants<-rbind(AIC.out,survey.like,survey.lambda,Lt.like,Lt.lambda,Age.like,Age.lambda,parms,dev.quants.labs)	
 	Like.parm.quants.table.data<-as_grouped_data(Like.parm.quants,groups=c("Label"))
